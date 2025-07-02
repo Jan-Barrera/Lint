c086532089c481a68d3a60a3280a9cf30e374773
Jan-Barrera <emmanueljan.barrera@cheetahdigital.com>
2025-07-02 10:21:40 +0800

    Initial Commit

---
+++ Lint/LintApp.swift
@@ -0,0 +1,17 @@
+//
+//  LintApp.swift
+//  Lint
+//
+//  Created by emmanueljan.barrera on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct LintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LintTests/LintTests.swift
@@ -0,0 +1,17 @@
+//
+//  LintTests.swift
+//  LintTests
+//
+//  Created by emmanueljan.barrera on 7/2/25.
+//
+
+import Testing
+@testable import Lint
+
+struct LintTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
